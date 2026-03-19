e6dd7df279b39e5a9b7ec9287083852f10cf1d66
Sain0225 <sainisaizal@gmail.com>
2026-03-19 14:40:52 -0400

    Test husky after removing tflint

---
+++ .github/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-rg"
+  location = "East US"
+}
